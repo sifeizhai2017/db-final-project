28068e27e33a75c725e6aa4b3743e19c188d7d00
tonghao12 <tonghao12@jd.com>
2020-05-29 01:36:05 +0800

    配置ehcache二级缓存

--- src/main/java/com/shnu/work/WorkApplication.java
+++ src/main/java/com/shnu/work/WorkApplication.java
@@ -2,8 +2,13 @@ package com.shnu.work;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * @author tonghao
+ */
 @SpringBootApplication
+@EnableCaching
 public class WorkApplication {
 
     public static void main(String[] args) {
