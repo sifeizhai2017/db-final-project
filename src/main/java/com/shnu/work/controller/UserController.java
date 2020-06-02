1f231ec91a88aec6d88b014e10927a08dfb4f595
tonghao12 <tonghao12@jd.com>
2020-06-02 18:08:07 +0800

    用户信息界面完成

--- src/main/java/com/shnu/work/controller/UserController.java
+++ src/main/java/com/shnu/work/controller/UserController.java
@@ -1,6 +1,16 @@
 package com.shnu.work.controller;
 
+import com.google.gson.Gson;
+import com.shnu.work.entity.UserInformationEntity;
+import com.shnu.work.service.IUserInformationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * 和用户相关的Controller
@@ -9,4 +19,24 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class UserController {
+    @Autowired
+    IUserInformationService userInformationService;
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
+    private final Gson gson = new Gson();
+
+    @RequestMapping("/getUserInfo")
+    public ModelAndView getUserInfo(ModelAndView modelAndView, HttpSession session) {
+        String loginUser = (String) session.getAttribute("login_user");
+        UserInformationEntity userInfo = userInformationService.getUserInformationEntityByUserAccount(loginUser);
+
+        modelAndView.setViewName("/userinfo");
+        return modelAndView;
+    }
+
+    @RequestMapping("/backToHomepage")
+    public String backToHomepage() {
+        return "/index";
+    }
 }
