61f70611081bfbb04a9bd702f5260d86b445b850
sifeizhai2017 <kiseryota2222@outlook.com>
2020-06-03 00:02:05 +0800

    引入校徽字体

--- src/main/java/com/shnu/work/controller/UserController.java
+++ src/main/java/com/shnu/work/controller/UserController.java
@@ -3,10 +3,12 @@ package com.shnu.work.controller;
 import com.google.gson.Gson;
 import com.shnu.work.entity.UserInformationEntity;
 import com.shnu.work.service.IUserInformationService;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -29,12 +31,16 @@ public class UserController {
     @RequestMapping("/getUserInfo")
     public ModelAndView getUserInfo(ModelAndView modelAndView, HttpSession session) {
         String loginUser = (String) session.getAttribute("login_user");
-        UserInformationEntity userInfo = userInformationService.getUserInformationEntityByUserAccount("woshinidie");
-
-        modelAndView.addObject("userInfo", userInfo);
+        if (!StringUtils.isBlank(loginUser)) {
+            UserInformationEntity userInfo = userInformationService.getUserInformationEntityByUserAccount(loginUser);
+            modelAndView.addObject("userInfo", userInfo);
+        }
         modelAndView.setViewName("/userinfo");
         return modelAndView;
     }
+
+//    @RequestMapping("/updateUserInfo")
+//    public ModelAndView
 
     @RequestMapping("/backToHomepage")
     public String backToHomepage() {
