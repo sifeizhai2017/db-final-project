89958309b12999eaa872ce324a00329c9888bdc9
tonghao12 <tonghao12@jd.com>
2020-06-08 11:00:40 +0800

    增加拦截器

---
+++ src/main/java/com/shnu/work/config/WebMvcConfiguration.java
@@ -0,0 +1,33 @@
+package com.shnu.work.config;
+
+import com.shnu.work.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Shinomiya Kaguya
+ */
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+        InterceptorRegistration interceptorRegistration = registry.addInterceptor(new LoginInterceptor());
+        interceptorRegistration.excludePathPatterns("/**/*.css",
+                "/**/*.jpeg",
+                "/**/*.jpg",
+                "/**/*.js",
+                "/**/*.png",
+                "/index");
+        interceptorRegistration.excludePathPatterns("/adminLogin",
+                "/login/**",
+                "/register/",
+                "/userLogin/",
+                ResourceUtils.CLASSPATH_URL_PREFIX + "/static/",
+                ResourceUtils.CLASSPATH_URL_PREFIX + "/templates/");
+        interceptorRegistration.addPathPatterns("/**");
+    }
+}
---
+++ src/main/java/com/shnu/work/interceptor/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package com.shnu.work.interceptor;
+
+import com.shnu.work.util.NewRedisUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 登录拦截器
+ *
+ * @author Shinomiya Kaguya
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    private final static Logger LOGGER = LoggerFactory.getLogger(LoginInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        NewRedisUtils newRedisUtils = NewRedisUtils.getRedisUtil();
+        HttpSession session = request.getSession();
+        String jSessionId = session.getId();
+        LOGGER.info("jsessionid:{}", jSessionId);
+        String userInfo;
+        try {
+            userInfo = newRedisUtils.get(0, jSessionId);
+            LOGGER.info("userInfo:{}", userInfo);
+        } catch (Exception e) {
+            return true;
+        }
+        LOGGER.info("userInfo:{}", userInfo);
+
+        return userInfo != null;
+    }
+}
